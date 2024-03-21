ba633f0f3fd214271f965b6f63a5e2c8e24be0ac
gideon rotich <gideonrotich2001@gmail.com>
2024-03-21 13:20:10 +0300

    good

--- index.js
+++ index.js
@@ -5,31 +5,94 @@ function printValue(value){
 
 // 1.0 FUNCTION DECLARATION
 //TODO: create a basic function that adds two random values
+  //we have two toys.....toy 1 = car toy 2 = blocks>>>>>count total toys
 
+  function addFixedValues(){
+    const toycar = 10; //The no of toycars we have
+    const toyblock = 7; //The number of block we have
+
+    const sum = toyCar + toyBlock //add our toys together
+    
+    //print our results
+    printValue(sum);
+  }
 
 //TODO: create a function that takes in two values and adds them
+  //count our toys
+  function addTwoValues(x, y){
+    //we are adding our two values together
+    const sum = x + y;
 
+    //print our results
+    printValue(sum);
+  }
 
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
-
+ //we want to show someone else our results
+ function addTwoValuesAndReturn(x, y){
+    //we are adding our two values together
+    const sum = x + y;
+    //Return results so that msee mwingine azione
+ }
 
 // 1.0 FUNCTION EXPRESSION
 //TODO: create a basic function that adds two random values
 
+const addFixedValuesExpr function (){
+    const toyCar = 10;
+    const toyBlock = 7;
+    const sum = toyCar + toyBlock;
+
+    //print our results
+    printValue(sum);
+}
 
 //TODO: create a function that takes in two values and adds them
+const adTwoValuesExpr = function (x, y){
 
+    const sum = x + y;
+
+    printValue(sum);
+}
 
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
 
+const addTwoValuesAndReturnExpr = function(x, y){
 
+    const sum =x + y;
+
+    Return sum;
+}
 // 1.0 ARROW FUNCTION
 //TODO: create a basic function that adds two random values
 
+const addFixedValuesArrow = () =>{
+    const toyCar = 10;
+    const toyBlock = 7;
+    const sum = toyCar + toyBlock;
+
+    //print our results
+    printValue(sum);
+}
 
 //TODO: create a function that takes in two values and adds them
 
+const addTwoValuesArrow = (x, y) =>{
+    const sum = x + y;
+
+    printValue(sum);
+}
 
 //TODO: create a function that takes in two values, adds them and returns the value of the addition
+const addTwoValuesAndReturnArrow = (x, y) =>{
+
+    const sum = x+ y;
+
+    return sum;
+}
+
+//testing our functions
+
+
 
 
